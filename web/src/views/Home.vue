fcdff4ea4ba6719198ac745b54bd951e0e6054f1
guoshuang <2072926629@qq.com>
2020-04-17 11:01:03 +0800

    web

---
+++ web/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    首页
+     <el-link href="file:///D:/Wis/hsop/src/assets/img/issue.png"  target="_blank">默认链接</el-link>
+     <a href="file:///D:/Wis/hsop/src/assets/img/issue.png" @click='this.window.open("file:///D:/Wis/hsop/src/assets/img/issue.png")' download="file:///D:/Wis/hsop/src/assets/img/issue.png">下载</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div class="topbar bg-black py-2 px-3 d-flex ai-between">
+          <img src="../assets/logo.png" height="30" alt="666">
+          <div class="px-3">
+              <div class="text-white">王者荣耀</div>
+              <div class="text-dark-1 fx-xs">团队成就更多</div>
+          </div>
+          
+      </div>
+      <div class="nav">
+          nav
+      </div>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
